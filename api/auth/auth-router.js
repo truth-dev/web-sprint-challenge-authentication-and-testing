7ea824ecb3f23acbc9e1683c449d24f478d5e01b
truth-dev <Truth8510@hotmail.com>
2023-12-31 17:17:23 -0700

    possibly fixed

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -8,60 +8,63 @@ const jwt = require("jsonwebtoken");
 const { BCRYPT_ROUNDS, JWT_SECRET } = require("../../config/index");
 
 router.post("/register", async (req, res, next) => {
+try{
   const { username, password } = req.body;
-  const hash = bcrypt.hashSync(password, BCRYPT_ROUNDS);
-  const user = { username, password: hash };
-  try {
-    const newUser = await Users.add(user);
-    res.status(201).json({
-      message: `Welcome ${newUser.username}!`,
-      id: newUser.id,
-      username: newUser.username,
-    });
+  const user = await Users.findBy({ username }).first();
+
     if (!username || !password) {
-      res.status(400).json({
-        message: "Please provide a username and password",
-     
+      return res.status(400).json({
+        message: "username and password required",
       });
-    } else {
-      next();
-     }
-     if(newUser){
-      res.status(422).json({message: "Username taken"})
-    }
-    }
-    catch (err) {
-    next(err);
   }
-  
-
-
-
-  
+  if (user) {
+    return res.status(409).json({
+      message: "username taken",
+    });
  
+  }
+  const newUser = await Users.add({
+    username,
+    password: await bcrypt.hash(password, BCRYPT_ROUNDS),
+  });
+  res.status(201).json(newUser);
+}catch(err){
+  next(err);
 
-
+}
+  
 });
 
 
 
 
     
-router.post("/login", (req, res, next) => {
+router.post("/login", async (req, res, next) => {
+try {
   const { username, password } = req.body;
+  const user = await Users.findBy({ username }).first();
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid Credentials",
+    });
+  }
+  const passwordValid = await bcrypt.compare(password, user.password);
+  if (!passwordValid) {
+    return res.status(401).json({
+      message: "Invalid Credentials",
+    });
+  }
+  const token = buildToken(user);
+  res.status(200).json({
+    message: `Welcome ${user.username}!`,
+    token,
+  });
+}catch(err){
+  next(err);
 
-  Users.findBy({ username })
-    .then((user) => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = buildToken(user);
-        res
-          .status(200)
-          .json({ message: `Welcome back ${user.username}...`, token });
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch(next);
+}
+
+ 
 });
 
 function buildToken(user) {
